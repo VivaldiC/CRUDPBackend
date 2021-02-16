24567a788fdcfb4ab021110311f47e34f0017c24
CarlosLara135 <carlos.lara@upnay.edu.mx>
2021-02-16 16:06:36 -0700

    App notes updated v 0.2

--- CRUDProjectV2/notes/models.py
+++ CRUDProjectV2/notes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Notes(models.Model):
+    task_title = models.CharField("Task_Title", max_length=255)
+    description = models.TextField(blank=True, null=True)
+    createdAt = models.DateTimeField("Created At", auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
--- CRUDProjectV2/notes/views.py
+++ CRUDProjectV2/notes/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Notes
+from .serializers import *
+
+@api_view(['GET', 'POST'])
+def notes_list(request):
+    """
+ List  notes, or create a new note.
+ """
+    if request.method == 'GET':
+        data = []
+        nextPage = 1
+        previousPage = 1
+        notes = Notes.objects.all()
+        page = request.GET.get('page', 1)
+        paginator = Paginator(notes, 5)
+        try:
+            data = paginator.page(page)
+        except PageNotAnInteger:
+            data = paginator.page(1)
+        except EmptyPage:
+            data = paginator.page(paginator.num_pages)
+
+        serializer = NotesSerializer(data,context={'request': request} ,many=True)
+        if data.has_next():
+            nextPage = data.next_page_number()
+        if data.has_previous():
+            previousPage = data.previous_page_number()
+
+        return Response({'data': serializer.data , 'count': paginator.count, 'numpages' : paginator.num_pages, 'nextlink': '/api/notes/?page=' + str(nextPage), 'prevlink': '/api/notes/?page=' + str(previousPage)})
+
+    elif request.method == 'POST':
+        serializer = NotesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def notes_detail(request, pk):
+    """
+ Retrieve, update or delete a note by id/pk.
+ """
+    try:
+        note = Note.objects.get(pk=pk)
+    except Note.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = NoteSerializer(note,context={'request': request})
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = NoteSerializer(note, data=request.data,context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
